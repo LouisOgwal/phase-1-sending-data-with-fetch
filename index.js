5390f1099aef0182a4da2ed33405060468598f97
louisogwal <yienlouis470@gmail.com>
2024-10-14 11:26:26 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+
+        const newId = document.createElement('p');
+        newId.textContent = `User ID: ${object.id}`;
+        document.body.appendChild(newId);
+      })
+      .catch(function (error) {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorMessage);
+      });
+  }
